e2a97544063a5060223e248ed46c1f0fbce16c41
lohith-ar <83833189+lohith-ar@users.noreply.github.com>
2021-06-25 20:37:53 +0530

    Add files via upload

---
+++ user-management-service/src/main/java/com/stackroute/usermanagementservice/config/ApiConfig.java
@@ -0,0 +1,27 @@
+package com.stackroute.usermanagementservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+@Configuration
+@EnableSwagger2
+public class ApiConfig {
+    @Bean
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Pandoptimist").apiInfo(apiInfo()).select()
+                .paths(regex("/api.*")).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("user-management-service")
+                .description("Documentation Generated Using SWAGGER2 for the user-management-service Rest API's")
+                .build();
+    }
+}
